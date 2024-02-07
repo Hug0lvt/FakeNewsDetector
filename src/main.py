a41ee5e5fdd7b819bef964021c84fd3edb4a7419
Axel DE LA FUENTE <axel.dlf@free.fr>
2024-02-07 11:06:58 +0100

    :truck: add initial files for ai

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+import preprocessing
+import classifier
+import analysis
+
 if __name__ == '__main__':
-    print("Start learning...")
+    print("Start learning... :)")
+
+    df = preprocessing.load_datas()
+    df.head(5)
---
+++ src/preprocessing.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def load_datas():
+    return pd.read_csv("../datas/FakeNewsNet.csv")
