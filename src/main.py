140e2dd5c76c383a802fc6102fe92672b71d8673
axdelafuen <axel.dlf@free.fr>
2024-02-07 10:45:12 +0000

    :sparkles: transfer code from colab (traing model / metrics)

--- src/main.py
+++ src/main.py
@@ -2,8 +2,35 @@ import preprocessing
 import classifier
 import analysis
 
-if __name__ == '__main__':
-    print("Start learning... :)")
+from warnings import simplefilter
+simplefilter(action='ignore', category=FutureWarning)
 
-    df = preprocessing.load_datas()
-    df.head(5)
+if __name__ == '__main__':
+    print("Start learning...")
+
+    X_train, X_test, y_train, y_test = preprocessing.process()
+    print("\nPre-processing... OK")
+
+    print("\nTraining models...")
+    
+    y_pred_knn = classifier.knn_classifier(X_train, y_train, X_test)
+    print("Knn... OK")
+
+    y_pred_dt = classifier.decision_tree(X_train, y_train, X_test)
+    print("DecisionTree... OK")
+
+    y_pred_logistic_reg = classifier.logistic_regression(X_train, y_train, X_test)
+    print("Logistic Regression... OK")
+
+    y_pred_sgd = classifier.sgd_classifier(X_train, y_train, X_test)
+    print("SGD... OK")
+
+    print("\nMetrics calculations...")
+    
+    print("\n--------------Knn metrics---------------")
+    knn_accuracy, knn_conf_matrix, knn_class_report = analysis.metrics(y_test, y_pred_knn)
+    print(f'Accuracy: {knn_accuracy}')
+    print(f'Confusion Matrix:\n{knn_conf_matrix}')
+    print(f'Classification Report:\n{knn_class_report}')
+
+    print("\nTODO()")
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,46 @@
 import pandas as pd
 
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.model_selection import train_test_split
+
+def process():
+    df = load_datas()
+
+    df = tokenize_datas(df)
+
+    X, y = features_selection(df)
+
+    X_train, X_test, y_train, y_test = split_df(X, y)
+
+    return X_train, X_test, y_train, y_test 
+
+
 def load_datas():
     return pd.read_csv("../datas/FakeNewsNet.csv")
+
+def tokenize_datas(df):
+    le = LabelEncoder()
+    label = le.fit_transform(df['news_url'])
+    label1=le.fit_transform(df['title'])
+    label2=le.fit_transform(df['source_domain'])
+    df.drop("news_url", axis=1, inplace=True)
+    df.drop("title", axis=1, inplace=True)
+    df.drop("source_domain", axis=1, inplace=True)
+
+    df["news_url"] = label
+    df["title"] = label1
+    df["source_domain"] = label2
+
+    return df
+
+def features_selection(df):
+    features = ["title", "news_url", "source_domain"]
+
+    return df[features].fillna(''),  df["real"]
+
+
+def split_df(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.30, random_state=42)
+    return X_train, X_test, y_train, y_test
+
