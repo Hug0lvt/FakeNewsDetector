53ce483ffff9404f274cc64719da835040a6124c
axdelafuen <axel.dlf@free.fr>
2024-02-09 14:23:59 +0000

    :sparkles: :lipstick: add index viex for web app

--- src/app/src/views.py
+++ src/app/src/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Create your views here.
 
-from django.http import HttpResponse
+from .forms import TextForm
 
 def index(request):
-
-    
-
-    return HttpResponse("Hello, world. You're at the polls index.")
-    
+    if request.method == 'POST':
+        form = TextForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            url = form.cleaned_data["url"]
+            return redirect("index")  # Rediriger vers une page d'accueil ou une autre vue
+    else:
+        form = TextForm()
+    return render(request, 'home.html', {'form': form})
