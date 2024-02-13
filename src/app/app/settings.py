49a18ba82d2e6314d369a42e235bc072cb6e5484
axdelafuen <axel.de_la_fuente@etu.uca.fr>
2024-02-13 21:46:54 +0100

    :sparkles: get datas

--- src/app/app/settings.py
+++ src/app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9+n!natx3n$s!c64q&b#o2ji=-07gng^ncs94t&j6@5%)0qwx$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = preprocessing.process()
     print("\nPre-processing... OK")
 
-    run.run_knn(X_train, y_train, X_test, y_test)
+    #run.run_knn(X_train, y_train, X_test, y_test)
     #run.run_decision_tree(X_train, y_train, X_test, y_test)
     #run.run_logistic_regression(X_train, y_train, X_test, y_test)
     #run.run_stochastic_gradient_descent(X_train, y_train, X_test, y_test)
